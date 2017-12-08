a701545b4c4f2340117d0a16a364fb022c775bc8
mutalisk <0x08@0x08.net>
2017-12-08 11:13:13 +0800

    BUG FIXED

--- src/pow.cpp
+++ src/pow.cpp
@@ -94,12 +94,13 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&
     bool fOverflow;
     arith_uint256 bnTarget;
 
-	if (pindexBestHeader) {
-		if ((pindexBestHeader->nHeight >= Params().GetConsensus().UBCHeight - 1) 
-			&& (pindexBestHeader->nHeight < (Params().GetConsensus().UBCHeight + Params().GetConsensus().UBCInitBlockCount - 1)))
+	CBlockIndex* pblockindex =chainActive.Tip();
+
+	if (pblockindex) {
+		if ((pblockindex->nHeight >= Params().GetConsensus().UBCHeight -1) 
+			&& (pblockindex->nHeight < (Params().GetConsensus().UBCHeight + Params().GetConsensus().UBCInitBlockCount -1)))
 			return true;	
 	}
-
     bnTarget.SetCompact(nBits, &fNegative, &fOverflow);
 
     // Check range
