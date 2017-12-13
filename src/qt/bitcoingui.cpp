252e3a885d2071efe56c788cb4b65d274c14ec8a
BlockShakalaka <leo@ub.com>
2017-12-13 11:29:36 +0800

    change default path and change default regedit settings

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -878,7 +878,7 @@ void BitcoinGUI::setNumBlocks(int count, const QDateTime& blockDate, double nVer
 
 void BitcoinGUI::message(const QString &title, const QString &message, unsigned int style, bool *ret)
 {
-    QString strTitle = tr("Bitcoin"); // default title
+    QString strTitle = tr("UnitedBitcoin"); // default title
     // Default to information icon
     int nMBoxIcon = QMessageBox::Information;
     int nNotifyIcon = Notificator::Information;
--- src/util.cpp
+++ src/util.cpp
@@ -529,7 +529,7 @@ fs::path GetDefaultDataDir()
     // Unix: ~/.bitcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "UnitedBitcoin";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -539,10 +539,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/UnitedBitcoin";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".unitedbitcoin";
 #endif
 #endif
 }
