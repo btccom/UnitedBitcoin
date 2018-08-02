1217fde5245e4d386bd9745601cd41d5e16d9f9e
12henry <12henry@ub.com>
2018-08-02 15:05:25 +0800

    add getposstate rpc interface

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -2,6 +2,7 @@
 // Copyright (c) 2009-2016 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#include <utilmoneystr.h>
 
 #include <base58.h>
 #include <amount.h>
